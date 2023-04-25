da7401d18ba2de2a30c7b05f295c2df22f0a06ee
Beatriz aquino <beatrizzoldyck07@gmail.com>
2023-04-24 22:06:38 -0400

    salvando commit

--- Pessoa.java
+++ Pessoa.java
@@ -1,5 +1,15 @@
 
 
 public class Pessoa{
-    
+
+    public String nome;
+    public Pessoa mae;
+    public Pessoa pai;
+
+    public void metodoConstrutor( String nome, Pessoa mae, Pessoa pai){
+        this.nome = nome;
+        this.mae = mae;
+        this.pai = pai;
+    } 
+
 }
