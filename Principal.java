3e72e6b9b84ce8d4650291ad3b115c02342bfae4
Beatriz aquino <beatrizzoldyck07@gmail.com>
2023-04-24 22:16:52 -0400

    corigindo

--- Principal.java
+++ Principal.java
@@ -4,9 +4,9 @@ public class Principal {
     public static void mian(String args[]){
         Pessoa mae = new Pessoa("Beatoriz", null, null);
         Pessoa pai = new Pessoa("Guilhermus", null, null);
-        Pessoa filhaA = new Pessoa("Evangeline", null, null);
-        Pessoa filhoB = new Pessoa("Harry", null, null);
+        Pessoa filhaA = new Pessoa("Evangeline", mae, pai);
+        Pessoa filhoB = new Pessoa("Harry", mae, pai);
 
-        filhaA.compare(filhoB);
+        System.out.println(filhaA.compara(filhoB));
     }
 }
